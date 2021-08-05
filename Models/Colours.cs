1b0b7dd3b81f75d4dfcf7976c90b697970ad355b
ITmbuyu <41137033+ITmbuyu@users.noreply.github.com>
2021-08-06 00:07:11 +0200

    testing

--- Models/Colours.cs
+++ Models/Colours.cs
@@ -10,5 +10,7 @@ namespace testinggit.Models
         public int ColoursId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Type { get; set; }
+        public string CookingName { get; set; }
     }
 }
